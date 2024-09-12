163751ed2734b3f6513a0e311dfc08273bbb2067
Anuar Zhangeldi <anuar1202@gmail.com>
2024-09-12 12:12:39 +0500

    refactoring of the test code

--- src/test/java/github/SoftAssertionsTests.java
+++ src/test/java/github/SoftAssertionsTests.java
@@ -5,9 +5,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.partialText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SoftAssertionsTests {
 
@@ -22,27 +21,31 @@ public class SoftAssertionsTests {
     void assertSelenidePageTest(){
 
         String JUnit5Code =
-                "@ExtendWith({SoftAssertsExtension.class})\n" +
-                        "class Tests {\n" +
-                        "  @Test\n" +
-                        "  void test() {\n" +
-                        "    Configuration.assertionMode = SOFT;\n" +
-                        "    open(\"page.html\");\n\n" +
-                        "    $(\"#first\").should(visible).click();\n" +
-                        "    $(\"#second\").should(visible).click();\n" +
-                        "  }\n" +
-                        "}";
+                """
+                        @ExtendWith({SoftAssertsExtension.class})
+                         class Tests {
+                           @Test
+                           void test() {
+                             Configuration.assertionMode = SOFT;
+                             open("page.html");
+                         
+                             $("#first").should(visible).click();
+                             $("#second").should(visible).click();
+                           }
+                         }
+                    """;
 
         open("/selenide/selenide");
 
         $("#wiki-tab").click();
-        $("button[data-view-component='true'].js-wiki-more-pages-link").click();
-// - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-        $("ul.page-navigation li").shouldHave(text("SoftAssertions"));
-    // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $$("li.Box-row.wiki-more-pages")
+                .filterBy(Condition.text("SoftAssertions"))
+                .first()
+                .shouldBe(Condition.visible);
         $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
-        $("div.highlight-source-java pre")
-                .shouldHave(Condition.text(JUnit5Code.trim()));
+        $("#wiki-body .highlight.highlight-source-java")
+                .shouldHave(partialText(JUnit5Code.trim()));
     }
 
 
