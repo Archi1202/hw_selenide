9459eda2b828baf56a1fd3bd9d92b0e41c9e5e8f
Anuar Zhangeldi <anuar1202@gmail.com>
2024-09-13 23:39:00 +0500

    Added moveElementsSuccessTest method

--- src/test/java/com/herokuapp/theinternet/DragAndDropTests.java
+++ src/test/java/com/herokuapp/theinternet/DragAndDropTests.java
@@ -1,26 +1,42 @@
 package com.herokuapp.theinternet;
 
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDropTests {
-
+@BeforeAll
+static void beforeAll(){
+    Configuration.browserSize = "1920x1080";
+    Configuration.baseUrl = "https://the-internet.herokuapp.com";
+    Configuration.pageLoadStrategy = "eager";
+}
     @Test
     void dragAndDropSuccessTest(){
-        open("https://the-internet.herokuapp.com/drag_and_drop");
+        open("/drag_and_drop");
 
-        // Locate the rectangles
-        SelenideElement columnA = $("#column-a");
-        SelenideElement columnB = $("#column-b");
+        actions()
+                .dragAndDrop($("#column-a"),$("#column-b"))
+                .perform();
 
-        // Perform drag and drop using Actions
-        actions().dragAndDrop(columnA, columnB).perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+    @Test
+    void moveElementsSuccessTest(){
+        open("/drag_and_drop");
 
-        // Verify the rectangles are swapped
-        columnA.shouldHave(text("B"));
-        columnB.shouldHave(text("A"));
+        actions()
+                .moveToElement($("#column-a"))
+                .clickAndHold()
+                .moveToElement($("#column-b"))
+                .release()
+                .perform();
+
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
     }
 }
